21b7103d518539bc0b6b498e6f7521c87d8d2e57
artym <artym.surkov@mail.ru>
2024-06-30 21:12:59 +0500

    Задача "Угадай функцию"

---
+++ src/main/java/org/surkov/Season2/GuessFunction.java
@@ -0,0 +1,43 @@
+package org.surkov.Season2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * Собрались как-то круги, формула, теорема и олимпиада и решили посчитать функцию. К сожалению, им
+ * нужно было очень спешить на YoungCon,поэтому они успели посчитать только первые 10 значений. Они
+ * очень хотят подарить по значению функции всем участникам конференции, поэтому просят вас помочь
+ * посчитать их для различных n. Помогите ребятам порадовать всех на конференции!
+ */
+public class GuessFunction {
+
+  public static long phi(long n) {
+    long result = n;
+    for (long i = 2; i * i <= n; ++i) {
+      if (n % i == 0) {
+        while (n % i == 0) {
+          n /= i;
+        }
+        result -= result / i;
+      }
+    }
+    if (n > 1) {
+      result -= result / n;
+    }
+    return result;
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(reader.readLine());
+    writer.write(String.valueOf(phi(n)));
+
+    reader.close();
+    writer.close();
+  }
+}
